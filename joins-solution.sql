525fa63fc0950f3c6a0a9550c3903abd0f725548
Cody Troop <codytroop@yahoo.com>
2019-02-06 08:37:48 -0600

    Assignment complete

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,27 +1,68 @@
+
+
 --1. Get all customers and their addresses.
+SELECT first_name, last_name, addresses.* FROM customers
+JOIN addresses ON customers.id=addresses.customer_id;
 
 --2. Get all orders and their line items (orders, quantity and product).
+SELECT orders.id AS "order id", line_items.quantity, products.description FROM orders
+JOIN line_items ON orders.id = line_items.order_id
+JOIN products ON products.id = line_items.product_id;
 
 --3. Which warehouses have cheetos?
+SELECT warehouse FROM warehouse
+JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
+JOIN products ON products.id = warehouse_product.product_id
+WHERE warehouse_product.product_id = 5;
 
 --4. Which warehouses have diet pepsi?
+SELECT warehouse FROM warehouse
+JOIN warehouse_product ON warehouse.id = warehouse_product.warehouse_id
+JOIN products ON products.id = warehouse_product.product_id
+WHERE warehouse_product.product_id = 6;
 
 --5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT first_name, last_name, count(customers.id) FROM customers
+JOIN addresses ON customers.id=addresses.customer_id
+JOIN orders ON orders.address_id = addresses.id
+GROUP BY customers.id;
 
 --6. How many customers do we have?
+SELECT count(id) FROM customers;
 
 --7. How many products do we carry?
+SELECT count(id) FROM products;
 
 --8. What is the total available on-hand quantity of diet pepsi?
-
+SELECT sum(warehouse_product.on_hand) FROM warehouse_product
+JOIN products ON products.id = warehouse_product.product_id
+WHERE warehouse_product.product_id = 6;
 
 --////////////////////////////////////////////////////////////
 
 --Stretch
 
 --9. How much was the total cost for each order?
+SELECT orders.id, sum(line_items.quantity*products.unit_price) AS "order cost" FROM orders
+JOIN line_items ON orders.id = line_items.order_id
+JOIN products ON line_items.product_id = products.id
+GROUP BY orders.id;
 
 --10. How much has each customer spent in total?
+SELECT customers.first_name, customers.last_name, 
+sum(line_items.quantity*products.unit_price) AS "customer spent"  FROM customers
+JOIN addresses ON customers.id = addresses.customer_id
+JOIN orders ON orders.address_id = addresses.id
+JOIN line_items ON orders.id = line_items.order_id
+JOIN products ON line_items.product_id = products.id
+GROUP BY customers.id;
 
 --11. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
-
+SELECT customers.first_name, customers.last_name, 
+COALESCE(sum(line_items.quantity*products.unit_price), 0) AS "customer spent"  
+FROM customers
+JOIN addresses ON customers.id = addresses.customer_id
+FULL OUTER JOIN orders ON orders.address_id = addresses.id
+FULL OUTER JOIN line_items ON orders.id = line_items.order_id
+FULL OUTER JOIN products ON line_items.product_id = products.id
+GROUP BY customers.id;
